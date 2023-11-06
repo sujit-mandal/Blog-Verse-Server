48c922e87301a9abafd638bccad9a7946f9e1517
Sujit Mandal <sm.bright22@gmal.com>
2023-11-07 00:10:19 +0600

    Added API endpoints for creating and retrieving blog posts

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+const moment = require("moment-timezone");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7djr5h.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7djr5h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,10 +29,38 @@ async function run() {
     const blogCollection = client.db("blogDB").collection("blogCollection");
 
     app.post("/api/v1/create-new-blog", async (req, res) => {
-      console.log(req.body);
+      const currentUTC = moment().utc();
+
+      // Convert the UTC time to Bangladeshi time (BST, UTC+6)
+      const addedTimeBD = currentUTC.tz("Asia/Dhaka");
       const blog = req.body;
+      blog.addedTime = addedTimeBD._d;
       const result = await blogCollection.insertOne(blog);
-      res.send(result)
+      res.send(result);
+    });
+
+    app.get("/api/v1/recent-blogs", async (req, res) => {
+      const result = await blogCollection
+        .find()
+        .sort({ addedTime: -1 })
+        .limit(6)
+        .toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/api/v1/all-blogs", async (req, res) => {
+      const searchQuery = req.query.q;
+      const category = req.query.category;
+      const query = category ? { category } : {};
+      if (searchQuery) {
+        const regex = new RegExp(searchQuery, "i");
+        query.title = { $regex: regex };
+      }
+      const result = await blogCollection.find(query).toArray();
+      console.log(searchQuery);
+      console.log(query);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
@@ -54,3 +81,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+

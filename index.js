933df5a34956d354fb842e51153308559de3ab07
Sujit Mandal <sm.bright22@gmal.com>
2023-11-08 19:33:22 +0600

    Implement API endpoint for removing a blog from wishlist

--- index.js
+++ index.js
@@ -130,12 +130,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/api/v1/blog/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await blogCollection.find(query).toArray();
-      res.send(result);
-    });
+    
 
     
     await client.db("admin").command({ ping: 1 });
